7cf5f9b92f50e34a4d0b876a4e94cdccf97533f0
Jack Gallagher <339690067@GoTVDSB.ca>
2024-10-23 09:39:20 -0400

    Class ended

--- Monogame Topic 4 - Time and Sound Assignment/Game1.cs
+++ Monogame Topic 4 - Time and Sound Assignment/Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Threading;
 
 namespace Monogame_Topic_4___Time_and_Sound_Assignment
 {
@@ -9,12 +11,20 @@ namespace Monogame_Topic_4___Time_and_Sound_Assignment
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        float seconds = 15;
+        bool bombExploded = false;
+
         Rectangle window;
 
-        Texture2D bombTexture;
-        Rectangle bombRect;
+        Texture2D bombTexture, explosionTexture;
+        Rectangle bombRect, explosionRect, mouseRect;
 
         SpriteFont timeFont;
+        SoundEffect explode;
+
+        SoundEffectInstance explodeSound;
+
+        MouseState mouseState;
 
         public Game1()
         {
@@ -27,6 +37,8 @@ namespace Monogame_Topic_4___Time_and_Sound_Assignment
         {
             // TODO: Add your initialization logic here
             bombRect = new Rectangle(50, 50, 700, 400);
+            explosionRect = new Rectangle(50, 50, 700, 400);
+            mouseRect = new Rectangle(0, 0, 1, 1);
             window = new Rectangle(0, 0, 800, 500);
             _graphics.PreferredBackBufferWidth = window.Width;
             _graphics.PreferredBackBufferHeight = window.Height;
@@ -42,6 +54,12 @@ namespace Monogame_Topic_4___Time_and_Sound_Assignment
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            bombTexture = Content.Load<Texture2D>("bomb");
+            explosionTexture = Content.Load<Texture2D>("bomb explosion");
+            timeFont = Content.Load<SpriteFont>("time");
+            explode = Content.Load<SoundEffect>("explosion");
+
+            explodeSound = explode.CreateInstance();
         }
 
         protected override void Update(GameTime gameTime)
@@ -50,8 +68,24 @@ namespace Monogame_Topic_4___Time_and_Sound_Assignment
                 Exit();
 
             // TODO: Add your update logic here
-            bombTexture = Content.Load<Texture2D>("bomb");
-            timeFont = Content.Load<SpriteFont>("time");
+            mouseRect.X = mouseState.X;
+            mouseRect.Y = mouseState.Y;
+
+            seconds -= (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (seconds <= 0)
+            {
+                if (!bombExploded)
+                    explodeSound.Play();
+
+                bombExploded = true;
+
+                if (explodeSound.State != SoundState.Playing)
+                    this.Exit();
+            }
+
+            mouseState = Mouse.GetState();
+            if (bombRect.Intersects(mouseRect) && mouseState.LeftButton == ButtonState.Pressed)
+                seconds = 15f;
 
             base.Update(gameTime);
         }
@@ -63,8 +97,15 @@ namespace Monogame_Topic_4___Time_and_Sound_Assignment
             // TODO: Add your drawing code here
             _spriteBatch.Begin();
 
-            _spriteBatch.Draw(bombTexture, bombRect, Color.White);
-            _spriteBatch.DrawString(timeFont, "1:00", new Vector2(270, 270), Color.Black);
+            if (!bombExploded)
+            {
+                _spriteBatch.Draw(bombTexture, bombRect, Color.White);
+                _spriteBatch.DrawString(timeFont, seconds.ToString("00.0"), new Vector2(270, 200), Color.Black);
+            }
+            else if (bombExploded)
+            {
+                _spriteBatch.Draw(explosionTexture, explosionRect, Color.White);
+            }
 
             _spriteBatch.End();
 
